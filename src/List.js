f1d083710bbc79df99d0fa6ca3588b6dfac7f1f7
Matthew England <MjE360@hotmail.co.uk>
2017-11-03 13:30:14 +0000

    To do app

---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+const List =(props) => (
+    <ul>
+      {props.items.map((items, index)=><li key={index}>{items}</li>)}
+      </ul>
+);
+
+
+
+
+
+
+export default List;
