a772320583a591886353e274c5b85559d2388e6c
Matthew England <MjE360@hotmail.co.uk>
2017-11-08 10:11:17 +0000

    style added

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,10 +1,35 @@
-import React  from 'react';
+import React from 'react';
 
-const List =(props) => (
-    <ul>
-      {props.items.map((items, index)=><li key={index}>{items.todo}<button onClick={()=>props.onComplete(index)}>{items.complete?'done':'todo'}</button></li>)}
+const staticStyle = {
+  margin: '10px 50px',
+}
+
+const List =(props) => {
+const style = {
+  complete:{
+    backgroundColor: 'white',
+    color: 'green',
+  },
+  incomplete:{
+    backgroundColor: 'white',
+    color: 'red'
+  }
+};
+
+const done = {
+  complete:{
+    textDecoration: 'line-through'
+  },
+  incomplete:{
+textDecoration: 'none'
+  }
+};
+
+  return(
+      <ul>
+        {props.items.map((items, index)=><li key={index} style={done[items.complete ? 'complete' : 'incomplete']}>{items.todo}<button style={{...staticStyle, ...style[items.complete ? 'complete' : 'incomplete'], ...props.styles}} onClick={()=>props.onComplete(index)}>{items.complete?'done':'todo'}</button></li>)}
       </ul>
-);
+)};
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import {purple300} from 'material-ui/styles/colors';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: 'black',
+  },
+  appBar: {
+    color: purple300,
+    height: 50
+  },
+  raisedButton: {
+    color: purple300,
+  },
+});
+
+const Page = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <AppBar title="My Todo App" />
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<Page />, document.getElementById('root'));
 registerServiceWorker();
